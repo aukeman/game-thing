72c9ba0520dc2bd3402397006b38d577eff86865
Jacob Aukeman <jacob@jacobaukeman.com>
2018-06-17 16:22:35 -0400

    garbage collect on every frame to aid reliably steady frame rates
    also sleep until toward the end of the frame, then start the busy loop

--- bin/screen_test.py
+++ bin/screen_test.py
@@ -7,9 +7,8 @@ resizable_mode=(pygame.HWSURFACE | pygame.RESIZABLE | pygame.DOUBLEBUF)
 
 screen_mode=fullscreen_mode
 
-#resolution=(256, 192)
-resolution=(640, 480)
-
+resolution=(256, 192)
+#resolution=(640, 480)
 
 def main():
     gc.disable()
@@ -22,10 +21,11 @@ def main():
     background.fill((255, 255, 255))
 
     font=pygame.font.Font(None, 36)
-    text=font.render('this is a test', True, (10, 10, 10))
-    
-    background.blit(text, (10,10))
 
+    scroller=font.render('this is a test', True, (10, 10, 10))
+    speed_pps=40.0
+    position=float(resolution[0])
+    
     #    screen.blit(background, (0,0))
     pygame.transform.scale(background, screen.get_size(), screen)
     pygame.display.flip()
@@ -35,6 +35,9 @@ def main():
     while True:
         clock.tick_busy_loop(60.0)
         top_of_frame=pygame.time.get_ticks()
+
+        if 17 < clock.get_time():
+            print('last frame took %d ms at %d' % (clock.get_time(), pygame.time.get_ticks()))
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -50,8 +53,14 @@ def main():
 
         background.fill((255, 255, 255))
         text=font.render('Frame Rate is %4.1f' % (clock.get_fps()), True, (10, 10, 10))
-#        text=font.render('Frame Time is %3.2f ms' % (clock.get_rawtime()), True, (10, 10, 10)) 
+#        text=font.render('Frame Time is %3.2f ms' % (clock.get_rawtime()), True, (10, 10, 10))
         background.blit(text, (10,10))
+
+        position = position-(speed_pps*clock.get_time()/1000.0)
+        if position < -scroller.get_width():
+            position = float(resolution[0])
+        background.blit(scroller, (int(position),100))
+        
         pygame.transform.scale(background, screen.get_size(), screen)
         pygame.display.flip()
     
